3ccaa8b06b0cc8b5ddb2bbb36edb521bf34f5411
Peter Ullrich <peda76@googlemail.com>
2018-01-10 17:01:39 +0100

    -BuiltInIntentHandler.js -BuiltInRequestHandler.js -Async removed. Now with promise :( AWS node is 6.1

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-module.exports = require('./lib/NodeSkill');
+module.exports.NodeSkill = require('./lib/NodeSkill');
+module.exports.AlexaRequest = require('./lib/AlexaRequest');
+module.exports.BuiltInIntentHandler = require('./lib/BuiltInIntentHandler');
+module.exports.BuiltInRequestHandler = require('./lib/BuiltInRequestHandler');
