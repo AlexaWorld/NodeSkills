fb39b18accf584f44b2f5b3a808e46e6516bd246
Peter Ullrich <peda76@googlemail.com>
2018-01-11 22:02:18 +0100

    -AlexaDate -AlexaDateParser

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ module.exports.NodeSkill = require('./lib/NodeSkill');
 module.exports.AlexaRequest = require('./lib/AlexaRequest');
 module.exports.BuiltInIntentHandler = require('./lib/BuiltInIntentHandler');
 module.exports.BuiltInRequestHandler = require('./lib/BuiltInRequestHandler');
+module.exports.AlexaDate = require('./lib/AlexaDate');
+module.exports.AlexaDateParser = require('./lib/AlexaDateParser');
+
