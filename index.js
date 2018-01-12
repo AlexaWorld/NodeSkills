e1a93595b0ba87b0ed6b8883b9840c1d69b705e3
Peter Ullrich <peda76@googlemail.com>
2018-01-12 17:47:58 +0100

    ?

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 module.exports.NodeSkill = require('./lib/NodeSkill');
 module.exports.AlexaRequest = require('./lib/AlexaRequest');
-module.exports.BuiltInIntentHandler = require('./lib/BuiltInIntentHandler');
-module.exports.BuiltInRequestHandler = require('./lib/BuiltInRequestHandler');
+//module.exports.BuiltInIntentHandler = require('./lib/BuiltInIntentHandler');
+m//odule.exports.BuiltInRequestHandler = require('./lib/BuiltInRequestHandler');
 module.exports.AlexaDate = require('./lib/AlexaDate');
 module.exports.AlexaDateParser = require('./lib/AlexaDateParser');
+module.exports.IntentHandler = require('./lib/IntentHandler');
+module.exports.RequestHandler = require('./lib/RequestHandler');
 
